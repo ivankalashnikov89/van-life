1ed4b05d8cede243f8584bbb58b6562c1f24e3a3
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-03-01 20:17:57 -0700

    add params2

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -11,15 +11,16 @@ export default function Vans() {
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
-            <img src={van.imageUrl} />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
-            </Link>      
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
     ))
+
 
     return (
         <div className="van-list-container">
