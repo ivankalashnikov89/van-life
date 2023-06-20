4c1a9cc7958cad1c6fd6eb8e08e554796092b962
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-06-19 23:45:59 -0600

    add Layout and Header

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+<header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
