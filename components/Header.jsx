4013b9513fc7de8f8bc6a31e16ca50ee8d23f6d9
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-06-20 00:09:03 -0600

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { Link } from "react-router-dom"
 
-<header>
+
+
+function Header() {
+    return (
+        <header>
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
           <Link to="/vans">Vans</Link>
         </nav>
       </header>
+    )
+}
+
+export default Header;
