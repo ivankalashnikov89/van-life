0b52b11956f80c3f88f357524386cb4c0547fce9
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-06-21 15:07:35 -0600

    Dashboard route

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,8 +1,13 @@
-import React from "react"
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 function Dashboard() {
     return (
-        <h1>Dashboard</h1>
+        <>
+            <h1>Dashboard</h1>
+            <Outlet />
+        </>
+        
     )
 }
 
