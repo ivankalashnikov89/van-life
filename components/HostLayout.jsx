9e660aa6d9d35d1830b2f97cd9efc9f28fcf8a71
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-06-21 15:47:54 -0600

    fixed

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -6,10 +6,10 @@ import { Link, Outlet } from "react-router-dom"
 function HostLayout() {
     return (
        <>
-        <nav>
+        <nav className="host-nav">
           <Link to="/host">Dashboard</Link>
           <Link to="/host/income">Income</Link>
-          <Link to="/vans/reviews">Reviews</Link>
+          <Link to="/host/reviews">Reviews</Link>
         </nav>
       <Outlet />
        </>
