cbb49ad7baf1c9b797d2575eb9d3cf546d7165b6
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-06-20 20:10:20 -0600

    added Host

--- index.jsx
+++ index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
 import Layout from "./components/Layout"
+import Dashboard from './pages/Host/Dashboard';
+import Income from './pages/Host/Income';
+import Reviews from './pages/Host/Reviews';
 
 import "./server"
 
@@ -15,10 +18,13 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
       </Route>   
-      <Route path="/about" element={<About />} />
     </Routes>
   </BrowserRouter>
   )
