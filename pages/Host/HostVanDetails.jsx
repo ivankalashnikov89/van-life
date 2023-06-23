5fa65f5ebb0db6f2d4043e0a77db46a6ff40bef4
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-06-23 16:04:12 -0600

    try to fix 33333

--- pages/Host/HostVanDetails.jsx
+++ pages/Host/HostVanDetails.jsx
@@ -1,9 +1,15 @@
 import React from "react"
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, NavLink, Outlet } from "react-router-dom"
 
-function HostVanDetails() {
+export default function HostVanDetail() {
     const { id } = useParams()
     const [currentVan, setCurrentVan] = React.useState(null)
+
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
 
     React.useEffect(() => {
         fetch(`/api/host/vans/${id}`)
@@ -35,9 +41,34 @@ function HostVanDetails() {
                         <h4>${currentVan.price}/day</h4>
                     </div>
                 </div>
+
+                <nav className="host-van-detail-nav">
+                    <NavLink
+                        to="."
+                        end
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Details
+                    </NavLink>
+
+                    <NavLink
+                        to="pricing"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Pricing
+                    </NavLink>
+
+                    <NavLink
+                        to="photos"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Photos
+                    </NavLink>
+
+                </nav>
+
+                <Outlet />
             </div>
         </section>
     )
 }
-
-export default HostVanDetails;
