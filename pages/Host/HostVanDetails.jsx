55c6d9b19acfdde86ab60f8ca8e7fc42b8d89f92
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-06-22 20:53:29 -0600

    fixed

--- pages/Host/HostVanDetails.jsx
+++ pages/Host/HostVanDetails.jsx
@@ -3,22 +3,22 @@ import { useParams } from "react-router-dom";
 
 function HostVanDetails() {
 
-    const {id} = useParams();
-    const[currentVan, setCurrentVan] = React.useState(null);
+    const { id } = useParams()
+    const [currentVan, setCurrentVan] = React.useState(null)
 
     React.useEffect(() => {
         fetch(`/api/host/vans/${id}`)
             .then(res => res.json())
             .then(data => setCurrentVan(data.vans))
-    },[])
-
-    if(!currentVan) {
+    }, [])
+    
+    if (!currentVan) {
         return <h1>Loading...</h1>
     }
 
     return (
         <div>
-            <img src={currentVan.imageURL} width={150}/>
+            <img src={currentVan.imageUrl} width={150} />
             <h2>{currentVan.name}</h2>
             <p>{currentVan.price}</p>
             <p>{currentVan.type}</p>
