d4b31e4a65b275a96f5a0400a00ba104fb3e523b
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-06-24 18:30:49 -0600

    rendering added

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -16,6 +16,7 @@ export default function Vans() {
     }, [])
     
 
+    /*Filter vans */
     const displayedVans = typeFilter 
     ? vans.filter(van => van.type.toLowerCase() === typeFilter)
     : vans
@@ -36,11 +37,18 @@ export default function Vans() {
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
+            /*Link to filtered lists */
             <div className="van-list-filter-buttons">
-                <Link to="?type=simple" className="van-type simple">Simple</Link>
-                <Link to="?type=luxury" className="van-type luxury">Luxury</Link>
-                <Link to="?type=rugged" className="van-type rugged">Rugged</Link>
-                <Link to="." className="van-type clear-filters">Clear filter</Link>
+                <Link to="?type=simple" className={
+                        `van-type simple ${typeFilter === "simple" ? "selected" : ""}`
+                    }>Simple</Link>
+                <Link to="?type=luxury" className={
+                        `van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`
+                    }>Luxury</Link>
+                <Link to="?type=rugged" className={
+                        `van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`
+                    }>Rugged</Link>
+                {typeFilter ? <Link to="." className="van-type clear-filters">Clear filter</Link> : null}
             </div>
             <div className="van-list">
                 {vanElements}
