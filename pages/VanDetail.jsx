03edf9ea4024ee0ec99baed8d06b307888861c33
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-06-24 18:50:47 -0600

    Update VanDetail.jsx

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 export default function VanDetail() {
     const params = useParams()
@@ -18,11 +18,13 @@ export default function VanDetail() {
                 relative="path"
                 className="back-button"
             >&larr; <span>Back to all vans</span></Link>
-
+            
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
-                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <i className={`van-type ${van.type} selected`}>
+                        {van.type}
+                    </i>
                     <h2>{van.name}</h2>
                     <p className="van-price"><span>${van.price}</span>/day</p>
                     <p>{van.description}</p>
