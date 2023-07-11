6497854e5aaff895e522791abdd1077cb0b3dc7b
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-07-11 14:53:33 -0600

    ProvideRouter added

--- index.jsx
+++ index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link
+} from "react-router-dom"
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
@@ -21,10 +27,9 @@ import "./server"
 
 
 function App() {
-  return (
-    <BrowserRouter>   
-      <Routes>
-        <Route path="/" element={<Layout />}>
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
@@ -42,8 +47,10 @@ function App() {
           </Route>
           <Route path="*" element={<NotFound />}/>
       </Route>   
-    </Routes>
-  </BrowserRouter>
+  ))
+
+  return (
+    <RouterProvider router={router} />
   )
 }
 
